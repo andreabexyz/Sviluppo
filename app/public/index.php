6506640c8c45beb2f24c245dcef2052323547237
Andrea Besso <andreabexyz@gmail.com>
2022-09-21 20:42:45 +0200

    Aggiunto scrittura su DB

--- app/public/index.php
+++ app/public/index.php
@@ -7,12 +7,55 @@ if (!$pdo) {
 }else{
     echo("Connessione riuscita.\n<br><br>");
 }
+$name="";
+$surname="";
+$nickName="";
 
-$query = $pdo->query('select nickname from users');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sviluppo</title>
+</head>
+<body>
+<h1>Lettura dal database Sviluppo</h1>
+<?php
+// Leggo da db
+    $query = $pdo->query('Select idUser, nickname FROM users');
+    
+    while($row = $query->fetch()){
+        echo (''. $row['idUser'].'° nickname: '. $row['nickname'].'<br>');   
+    }
 
-$count=1;
 
-while($row = $query->fetch()){
-    echo ($count.'° nickname: ' . $row['nickname'] . '<br>');   
-    ++$count;
-}
+?>
+<h1>Scrittura sul database Sviluppo</h1>
+    <form method="post" action="index.php">
+        <input type="text" id="name" name="name" placeholder="Nome"></input>
+        <input type="text" id="surname" name="surname" placeholder="Cognome"></input>
+        <input type="text" id="nickName" name="nickName" placeholder="Nickname"></input>
+        <input type="submit" id="submit" value="Aggiungi"></input>
+        <?php
+        // Recupero i valori inseriti nel form
+        if(isset($_POST['name'])){
+            $nome = $_POST['name'];
+        }
+        if(isset($_POST['surname'])){
+            $cognome = $_POST['surname'];
+        }
+        if(isset($_POST['nickName'])){
+            $soprannome = $_POST['nickName'];
+        }
+
+        if(!isset($nome) || !isset($cognome) || !isset($soprannome)){
+            echo '<br>Tutti i campi del modulo sono obbligatori!';    
+        }else{
+            $query2 = $pdo->query('Insert into users (name, surname, nickName) values("'.$nome.'","'.$cognome.'","'.$soprannome.'")');
+            echo("<br>Nuovo utente inserito correttamente!<br>");
+        }
+        ?>
+    </form>
+</body>
+<script>
+</script>
+</html>
